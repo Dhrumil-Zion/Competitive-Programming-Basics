aaf9b4ae5e92a1fba96157f687245170987c31d5
Dhrumil <dhrumilg699@gmail.com>
2021-03-08 16:07:37 +0530

    Apples_Orange_Problems  &  Grading_Problems

---
+++ Grading_Problem.py
@@ -0,0 +1,11 @@
+grades = [73,67,38,33]
+for x in range(len(grades)):
+        temp = grades[x] 
+        t=grades[x]
+        if grades[x]<38:
+            continue
+        while t%5!=0:
+           t +=1
+        if (t - temp) < 3:
+            grades[x]=t
+print(grades)
