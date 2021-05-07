cb2bc59854e1c6087ca21f52393fe5e91d05bcdb
Dhrumil <dhrumilg699@gmail.com>
2021-05-07 23:59:07 +0530

    optimized fibo problem

--- motified_fibonacci_problem.py
+++ motified_fibonacci_problem.py
@@ -2,10 +2,12 @@ n = 7
 one =1
 two = 2
 three = 3
-print(one,two,three)
-for c in range(3,n):
+tempp = [one, two, three]
+for _ in range(3,n):
     temp = one+two+three
-    print(temp)
+    tempp.append(temp)
     one = two 
     two = three
     three = temp
+
+print(tempp)
