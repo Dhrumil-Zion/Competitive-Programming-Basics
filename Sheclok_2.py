21f3b6e786e3b0e7292dd1985a16a7339daa700a
Dhrumil <dhrumilg699@gmail.com>
2021-05-20 15:35:55 +0530

    Structuring

--- Sheclok_2.py
+++ Sheclok_2.py
@@ -10,13 +10,5 @@ def squares(a, b):
     if d**2 == b:
         su+=1
     print(su)    
-    # c= 0 
-    # lis = [x for x in range(a,b+1)]
-    # z = [x for x in range(int(math.sqrt(a)),int(math.sqrt(b)+1))]
-    # for x in z:
-    #     if x**2 in lis:
-    #         c+=1
-    # print(c) 
 
 squares(3,9)    
-#squares(465868129,988379794)    
